e08e561919808e8b690134fe8beb3a4649c1a2f4
Ankur <ankur@BigAdmins-MacBook-Pro.local>
2019-06-02 17:30:45 +0530

    added problem 100. same_tree

---
+++ 100_same-tree/same-tree.go
@@ -0,0 +1,25 @@
+package same_tree
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	return strings.Compare(inOrder(p), inOrder(q)) == 0
+}
+
+func inOrder(node *TreeNode) string {
+	if node == nil {
+		return ""
+	}
+
+	return fmt.Sprintf("L(%s)", inOrder(node.Left)) + fmt.Sprintf("P(%s)", strconv.Itoa(node.Val)) + fmt.Sprintf("%s", inOrder(node.Right))
+}
