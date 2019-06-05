58d58c00b9d87da88bb0518a201a9480eae88295
Ankur <ankur@BigAdmins-MacBook-Pro.local>
2019-06-06 01:38:53 +0530

    added problem 404

---
+++ 404_sum-of-left-leaves/sum-of-left-leaves.go
@@ -0,0 +1,23 @@
+package binary_tree_paths
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if isLeaf(root.Left) {
+		return root.Left.Val + sumOfLeftLeaves(root.Right)
+	}
+
+	return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+}
+
+func isLeaf(node *TreeNode) bool {
+	return node != nil && node.Left == nil && node.Right == nil
+}
